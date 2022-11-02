3aa20dc4011728e648c2124aa72e553259b7df5e
SebastianSotoAr <ssotoa@unal.edu.co>
2022-11-01 20:59:48 -0500

    try 4

--- pregunta.py
+++ pregunta.py
@@ -7,19 +7,22 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import re
-from numpy import dtype
 import pandas as pd
 
 
 def clean_data():
 
+    #Limpia la fila monto_del_credito y la convierte en int
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
     df.monto_del_credito = df.monto_del_credito.str.strip("$")
     df.monto_del_credito = df.monto_del_credito.str.replace(",","")
     df.monto_del_credito = df.monto_del_credito.astype(float)
     df.monto_del_credito = df.monto_del_credito.astype(int)
 
+    #Comvierte la fila fecha_de_beneficio
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio)
+
     return df
 
 if __name__ == "__main__":
-    print(clean_data().monto_del_credito)
+    print(clean_data().fecha_de_beneficio)
