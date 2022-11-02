106eec163bdb34f533b03057c22ecbe576390b2f
SebastianSotoAr <ssotoa@unal.edu.co>
2022-11-01 22:16:58 -0500

    try 6

--- pregunta.py
+++ pregunta.py
@@ -12,8 +12,12 @@ import pandas as pd
 
 def clean_data():
 
-    #Limpia la fila monto_del_credito y la convierte en int
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+
+    df.dropna(axis=0, inplace=True)
+    df.drop_duplicates(inplace=True)
+
+    #Limpia la fila monto_del_credito y la convierte en int
     df.monto_del_credito = df.monto_del_credito.str.strip("$")
     df.monto_del_credito = df.monto_del_credito.str.replace(",","")
     df.monto_del_credito = df.monto_del_credito.astype(float)
@@ -25,14 +29,14 @@ def clean_data():
     #Limpia las filas sexo, tipo_de_emprendimiento, idea_negocio, línea_credito y barrio
     for filas in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
         df[filas] = df[filas].str.lower()
+        df[filas] = df[filas].str.replace('_', ' ')
+        df[filas] = df[filas].str.replace('-', ' ')
 
     return df
 
 if __name__ == "__main__":
     #print(list(filter(lambda data: ((data != "masculino") and (data != "femenino")), clean_data().sexo.str)))
-    print(clean_data().columns)
-    print(clean_data().dtypes)
-    print(clean_data().head())
+    print(clean_data())
     #for data in clean_data().monto_del_credito:
         #if ((re.match(r"\D", data))):
             #print(data)
