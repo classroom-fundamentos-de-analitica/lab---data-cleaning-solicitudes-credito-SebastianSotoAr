472070146ce239d9c2c716a5dc852395a7dd73eb
SebastianSotoAr <ssotoa@unal.edu.co>
2022-11-01 19:28:09 -0500

    try 1

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,24 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+import re
 import pandas as pd
 
 
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
+    df.monto_del_credito = df.monto_del_credito.str.strip("$")
+    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
+    df.monto_del_credito = df.monto_del_credito.astype(float)
 
     return df
+
+if __name__ == "__main__":
+    #print(list(filter(lambda data: re.match(r"\D", data), clean_data().monto_del_credito)))
+    #print("--------------------------------------")
+    #for data in clean_data().monto_del_credito:
+        #if ((re.match(r"\D", data))):
+            #print(data)
+
+    print(clean_data().monto_del_credito)
